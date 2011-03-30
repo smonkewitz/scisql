b7bb4db9f342a0a92ab49d8264aac4a0303ac313
Serge Monkewitz <smm@ipac.caltech.edu>
2011-03-29 21:52:05 -0500

    unit tests for median() and select()

--- test/__init__.py
+++ test/__init__.py
@@ -1,3 +1,6 @@
+from testMedian import MedianTestCase
+from testPercentile import PercentileTestCase
+
 from testAngSep import AngSepTestCase
 from testS2PtInBox import S2PtInBoxTestCase
 from testS2PtInCircle import S2PtInCircleTestCase
